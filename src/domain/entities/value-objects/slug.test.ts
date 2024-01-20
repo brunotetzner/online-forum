9dd739c05708383027dde0a5878bca37fbca6cb7
Bruno Tetzner <brunoltetzner@gmail.com>
2024-01-20 13:16:22 -0300

    feat: slug value object

---
+++ src/domain/entities/value-objects/slug.test.ts
@@ -0,0 +1,8 @@
+import { expect, test } from "vitest";
+import { Slug } from "./slug";
+
+test("It should be able to create a new slug from text", () => {
+  const slug = Slug.createFromText("How to create a slug");
+
+  expect(slug.value).toEqual("how-to-create-a-slug");
+});
---
+++ src/domain/entities/value-objects/slug.ts
@@ -0,0 +1,28 @@
+export class Slug {
+  public value: string;
+
+  constructor(value: string) {
+    this.value = value;
+  }
+
+  /**
+   *  Receveivas a string and normalizes it as a slug
+   *
+   * Exempla: "How to create a slug" -> "how-to-create-a-slug
+   *
+   * @param text
+   */
+  static createFromText(text: string) {
+    const slugText = text
+      .normalize("NFKD")
+      .toLocaleLowerCase()
+      .trim()
+      .replace(/\s+/g, "-")
+      .replace(/[^\w-]+/g, "")
+      .replace(/_/g, "-")
+      .replace(/--+/g, "-")
+      .replace(/-$/g, "");
+
+    return new Slug(slugText);
+  }
+}
