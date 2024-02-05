c8457821e41528a2f538cc3a331b74e587875300
Bruno Tetzner <brunoltetzner@gmail.com>
2024-02-05 19:47:32 -0300

    feat: delete question

--- src/domain/forum/application/repositories/question-repository.ts
+++ src/domain/forum/application/repositories/question-repository.ts
@@ -2,6 +2,8 @@ import { Question } from "../../enterprises/entities/question";
 import { Slug } from "../../enterprises/entities/value-objec/slug";
 
 export interface QuestionRepository {
+  findById(id: string): Promise<Question | null>;
   create(answer: Question): Promise<void>;
   findBySlug(slug: Slug): Promise<Question | null>;
+  delete(question: Question): Promise<void>;
 }
--- test/repositories/in-memory-questions-repository.ts
+++ test/repositories/in-memory-questions-repository.ts
@@ -5,6 +5,20 @@ import { Slug } from "@/domain/forum/enterprises/entities/value-objec/slug";
 export class InMemoryQuestionsRepository implements QuestionRepository {
   public items: Question[] = [];
 
+  async findById(id: string): Promise<Question | null> {
+    const question = this.items.find(
+      (question) => question.id.toString() === id
+    );
+
+    return question || null;
+  }
+
+  async delete(question: Question): Promise<void> {
+    const itemIdentifier = this.items.findIndex(
+      (item) => item.id === question.id
+    );
+    this.items.splice(itemIdentifier, 1);
+  }
   async create(question: Question): Promise<void> {
     this.items.push(question);
   }
